c4f5abaa9dab0f3f0e4117c5848adcbe621e695c
jonrhuang <jonrhuang@gmail.com>
2025-07-30 00:28:08 -0400

    Navbar & Styling My Solution

--- ReactFacts/src/components/Main.jsx
+++ ReactFacts/src/components/Main.jsx
@@ -1,5 +1,16 @@
 export default function Main() {
     return (
-        <h1>Main goes here</h1>
+        <div class="main-content">
+            <h1>Fun facts about React</h1>
+            <div>
+                <ul>
+                    <li>Was first released in 2013 </li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 200K stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+        </div>
     )
 }
--- ReactFacts/src/components/Navbar.jsx
+++ ReactFacts/src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 export default function Navbar() {
     return(
-        <h1>Navbar goes here</h1>
+        <header>
+            <nav class='nav-bar'>
+                <img src='src/assets/react-logo.png' alt='react-logo' size='40px'/>
+                <h2>ReactFacts</h2>
+            </nav>
+        </header> 
     )
 }
