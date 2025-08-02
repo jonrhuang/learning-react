5bbbfd19d74557e123ccd677cfe9dc42aa37769b
jonrhuang <jonrhuang@gmail.com>
2025-08-02 00:00:24 -0400

    ReactFacts project completed

--- ReactFacts/src/components/Main.jsx
+++ ReactFacts/src/components/Main.jsx
@@ -1,16 +1,14 @@
 export default function Main() {
     return (
-        <div class="main-content">
+        <main>
             <h1>Fun facts about React</h1>
-            <div>
-                <ul>
-                    <li>Was first released in 2013 </li>
-                    <li>Was originally created by Jordan Walke</li>
-                    <li>Has well over 200K stars on GitHub</li>
-                    <li>Is maintained by Meta</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
-            </div>
-        </div>
+            <ul className="facts-list">
+                <li>Was first released in 2013 </li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
